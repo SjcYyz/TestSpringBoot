0e4d32a30ff0c6ce37c434c552813dc1e3ac628f
SjcYyz <thbshenjc@tedu.cn>
2019-08-27 10:45:23 +0800

    去掉注释

--- SpringBoot/src/main/java/cn/tedu/com/Demo09.java
+++ SpringBoot/src/main/java/cn/tedu/com/Demo09.java
@@ -1,14 +1,13 @@
 package cn.tedu.com;
 
 /**
- * 1.这几天我好烦
+ * 
  * @author Sjc
  *
  */
 public class Demo09 {
 
 	public static void main(String[] args) {
-         
-		System.out.println("今天好热，哈哈哈哈哈哈");
+
 	}
 }
